cffb3f229bd4eeb73f552530a6e974381cb4d3c0
Gilbert Elnino <gilbeltelnino@gmail.com>
2020-03-27 11:21:25 +0200

    Updates

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,12 @@
-console.log("WELCOME");
+const menu = document.querySelector('#menu');
+const ul = document.querySelector('ul');
+const exit = document.querySelector('.exit-btn');
+
+const openMenu = ()=>{
+    ul.classList.toggle('hide-mobile');
+}
+const closeMenu = ()=>{
+    ul.classList.add('hide-mobile');
+}
+menu.addEventListener('click',openMenu);
+exit.addEventListener('click',closeMenu)
